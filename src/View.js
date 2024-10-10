9f0423524747dbc07de2939328bca7ce81cab2a4
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-10 18:17:59 +0900

    [refactor]: 줄바꿈 추가

--- src/View.js
+++ src/View.js
@@ -30,7 +30,7 @@ class BaseBallGameView {
 
   async promptRestartOrExit() {
     const userInput = await Console.readLineAsync(
-      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n'
     );
     validateRestartOrExit(userInput);
     return userInput;
