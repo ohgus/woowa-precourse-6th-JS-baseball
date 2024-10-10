059f68f9e2b179aa73a286ccafec4b47e68b2abe
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-10 17:01:52 +0900

    [feat]: 입력 결과 출력 로직 구현

--- src/View.js
+++ src/View.js
@@ -7,6 +7,22 @@ class BaseBallGameView {
     validateUserGuess(userInput);
     return userInput;
   }
+
+  displayResult(result) {
+    const { strike, ball } = result;
+
+    if (strike === 3) {
+      Console.print('3스트라이크');
+      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    } else if (strike === 0 && ball === 0) {
+      Console.print('낫싱');
+    } else {
+      let hint = '';
+      if (ball > 0) hint += `${ball}볼 `;
+      if (strike > 0) hint += `${strike}스트라이크`;
+      Console.print(hint.trim());
+    }
+  }
 }
 
 export default BaseBallGameView;
