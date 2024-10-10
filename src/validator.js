1d85dab8b8034627ba1c0f14fbf99979fd89aa24
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-10 16:20:48 +0900

    [feat]: 유저 입력 검증 로직 구현

---
+++ src/validator.js
@@ -0,0 +1,22 @@
+export const validateUserGuess = (userGuess) => {
+  if (userGuess.length !== 3) {
+    throw new Error('숫자는 3자리여야 합니다.');
+  }
+
+  const userGuessArray = userGuess.split('');
+
+  const isAllNumbers = userGuessArray.every((char) => !isNaN(char));
+  if (!isAllNumbers) {
+    throw new Error('숫자만 입력할 수 있습니다.');
+  }
+
+  const isAllUnique = new Set(userGuessArray).size === userGuessArray.length;
+  if (!isAllUnique) {
+    throw new Error('중복되지 않는 숫자만 입력할 수 있습니다.');
+  }
+
+  const isAllInRange = userGuessArray.every((char) => char >= 1 && char <= 9);
+  if (!isAllInRange) {
+    throw new Error('1~9 사이의 숫자만 입력할 수 있습니다.');
+  }
+};
