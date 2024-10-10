30b6e20c82512e32c028f3d8089613a9ebedaed2
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-10 16:37:36 +0900

    [refactor]: 입력 값 검증이 완료되고 값을 반환하게 수정

--- src/View.js
+++ src/View.js
@@ -1,8 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
+import { validateUserGuess } from './validator.js';
 
 class BaseBallGameView {
   async promptUserGuess() {
     const userInput = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    validateUserGuess(userInput);
     return userInput;
   }
 }
