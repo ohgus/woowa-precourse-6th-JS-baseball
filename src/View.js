29e27df8795df1b62ceea7b799f03922416af821
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-10 17:15:29 +0900

    [feat]: 게임 시작 메세지 출력 로직 구현

--- src/View.js
+++ src/View.js
@@ -2,6 +2,10 @@ import { Console } from '@woowacourse/mission-utils';
 import { validateUserGuess, validateRestartOrExit } from './validator.js';
 
 class BaseBallGameView {
+  printStartMessage() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
   async promptUserGuess() {
     const userInput = await Console.readLineAsync('숫자를 입력해주세요 : ');
     validateUserGuess(userInput);
