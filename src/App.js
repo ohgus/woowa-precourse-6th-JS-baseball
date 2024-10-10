6dc6f0489f0b6ba72d9c22e45f695e6d00bb277f
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-10 18:20:38 +0900

    [chore]: 컨트롤러 앱에 연동

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
+import BaseBallGameController from './Controller.js';
 
 class App {
-  async play() {}
+  constructor() {
+    this.controller = new BaseBallGameController();
+  }
+
+  async play() {
+    this.controller.startGame();
+  }
 }
 
 export default App;
+
+const app = new App();
+app.play();
