8fe06819d706f29a1657e295b0e708d702600a19
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-10 22:33:41 +0900

    [refactor]: 각각의 검증 로직을 분리

--- src/validator.js
+++ src/validator.js
@@ -1,24 +1,34 @@
-export const validateUserGuess = (userGuess) => {
+const isCorrectLength = (userGuess) => {
   if (userGuess.length !== 3) {
     throw new Error('[ERROR] 숫자는 3자리여야 합니다.');
   }
+};
 
-  const userGuessArray = userGuess.split('');
-
+const isAllNumbers = (userGuessArray) => {
   const isAllNumbers = userGuessArray.every((char) => !isNaN(char));
   if (!isAllNumbers) {
     throw new Error('[ERROR] 숫자만 입력할 수 있습니다.');
   }
+};
 
-  const isAllUnique = new Set(userGuessArray).size === userGuessArray.length;
-  if (!isAllUnique) {
+const isAllUnique = (userGuessArray) => {
+  if (new Set(userGuessArray).size !== userGuessArray.length) {
     throw new Error('[ERROR] 중복되지 않는 숫자만 입력할 수 있습니다.');
   }
+};
 
-  const isAllInRange = userGuessArray.every((char) => char >= 1 && char <= 9);
-  if (!isAllInRange) {
+const isAllInRange = (userGuessArray) => {
+  if (!userGuessArray.every((char) => char >= 1 && char <= 9)) {
     throw new Error('[ERROR] 1~9 사이의 숫자만 입력할 수 있습니다.');
   }
+};
+
+export const validateUserGuess = (userGuess) => {
+  isCorrectLength(userGuess);
+  const userGuessArray = userGuess.split('');
+  isAllNumbers(userGuessArray);
+  isAllUnique(userGuessArray);
+  isAllInRange(userGuessArray);
 };
 
 export const validateRestartOrExit = (userInput) => {
